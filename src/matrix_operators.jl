d950256f24e4333a5f78861008c889a5788fc27d
Diederik Beckers <beckersdiederik@gmail.com>
2022-05-03 17:02:39 -0700

    Updated docstring of create_RTLinvR

--- src/matrix_operators.jl
+++ src/matrix_operators.jl
@@ -1,8 +1,7 @@
 """
     create_RTLinvR(cache::BasicILMCache[;scale=1.0])
 
-Using the provided cache `cache`, construct the square matrix ``-R^T L^{-1}R``, which maps data of type `ScalarData`
-to data of the same type. The operators `R^T` and `R` correspond to [`interpolate!`](@ref) and
+Using the provided cache `cache`, construct the square matrix ``-R^T L^{-1}R``, which maps data of the primary point data type of the cache to data of the same type. The operators `R^T` and `R` correspond to [`interpolate!`](@ref) and
 [`regularize!`](@ref) and `L^{-1}` is the inverse of the grid Laplacian. The optional keyword `scale` multiplies the
 matrix by the designated value.
 """
